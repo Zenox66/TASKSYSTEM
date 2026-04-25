747c5c3045066437a99668964c7dd55c72496b3c
Zenox66 <stephenreyno@gmail.com>
2026-04-25 13:56:14 +0800

    .

---
+++ app/Views/layout/app.php
@@ -0,0 +1,12 @@
+<?php $config = require __DIR__ . '/../../Config/Config.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require $contentView;?>
+</body>
+</html>
