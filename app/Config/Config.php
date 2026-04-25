7a34846f73faf6252d2ad5e822f846f6b0b93ea4
Zenox66 <stephenreyno@gmail.com>
2026-04-25 14:28:43 +0800

    .

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -1,7 +1,6 @@
 <?php
 return[
     'app_name' => 'Task Management System',
-    'app_url' => 'https://'. $_SERVER['HTTP_HOST'] . '/TASKSYSTEM/public',
-    public,
+    'app_url' => 'https://'. $_SERVER['HTTP_HOST'] . '/tasksystem/public',
 ];
 ?>
